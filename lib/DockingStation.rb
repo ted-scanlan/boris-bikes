1e990b8092292aed17149111e65a18a21a3f4734
Student <student@makerss-mbp.lan>
2019-06-27 23:24:00 +0100

    up to stage 16

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,13 +1,39 @@
 class DockingStation
   attr_reader :bike
+
+def initialize
+@dock = []
+@DEFAULT_CAPACITY = 20
+end
+
 def release_bike
-raise "no bikes available" unless @bike
-@bike 
+raise "no bikes available" if @dock.empty?
+@dock.pop
 end
+
+
 def dock_bike(bike)
-  @bike = bike
+
+raise "docking station full" if full?      #@dock.count >= 20
+@dock.push(bike)
+
 end
-#def bike
-#  @bike
+
+def bike
+ @dock
+end
+
+
+private
+def full?
+  if @dock.count >= @DEFAULT_CAPACITY
+    return true
+  else
+    return false
+  end
+
+end
+#def stored_bike
+#@bike
 #end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,44 +1,49 @@
 require 'DockingStation.rb'
+require 'bike.rb'
 
 describe DockingStation do
   it 'responds to the method "release_bike"' do
     expect(subject).to respond_to(:release_bike)
   end
 
-#  it 'creates a bike' do
-#      bike = subject.release_bike
-#      expect(bike).to be_working   #this wont work now because release_bike doesnt create a new instance of bike class, it just gives us the bike we've already created when we docked
-#end
+  #  it 'creates a bike' do
+  #      bike = subject.release_bike
+  #      expect(bike).to be_working   #this wont work now because release_bike doesnt create a new instance of bike class, it just gives us the bike we've already created when we docked
+  #end
 
-describe '#release_bike' do   #  put a # in front when referring to an instance method
+  describe '#release_bike' do   #  put a # in front when referring to an instance method
 
-  it 'raises error when there are no bikes available' do
-    expect {subject.release_bike}.to raise_error 'no bikes available'
+    it 'raises error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'no bikes available'
+    end
+    it 'it raises error when docking station is full' do
+      bike = Bike.new
+      20.times {subject.dock_bike(bike)}
+      expect {subject.dock_bike(bike)}.to raise_error 'docking station full'
+    end
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike  # release the bike we docked
+    end
+
   end
-  it 'releases a bike' do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect(subject.release_bike).to eq bike  # release the bike we docked
-   end
 
-end
-
+  describe "#dock_bike" do
 
   it 'responds to the method "dock_bike"' do
     expect(subject).to respond_to(:dock_bike).with(1).argument
-end
-it 'docks a bike' do
-  bike = Bike.new
-  expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  it 'returns a docked bike stored in an array' do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq [bike]
+  end
+
 end
 
-it 'responds to the method "bike"' do # this will return to use a stored bike
-  expect(subject).to respond_to(:bike)
-end
-it 'shows us a bike' do
-  bike = Bike.new
-  subject.dock_bike(bike)
-  expect(subject.bike).to eq bike
-end
+  it 'responds to the method "bike"' do # this will return to use a stored bike
+    expect(subject).to respond_to(:bike)
 
+  end
 end
